9dbc23a923ce14ded410ac88abd09e962c0c99a2
Agnes <koinageagness@gmail.com>
2022-03-04 23:15:37 +0300

    add folder 'images

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 
 //arrays of days of the week, male names and female names
-let daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 
-let akanMaleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var akanMaleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 
-let akanFemaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+var akanFemaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
+function checkAkanName(){
+    var YY= document.getElementById("year").value;
+    var MM = Number(document.getElementById("month").value);
+    var DD = Number(document.getElementById("day").value);
+    var gender = document.getElementsByName("gender");
+    var CC = parseInt(year.substring(0,2));
+    var (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+}
+
+function getGender() {
+    if(gender.option==true){
+        return gender="female"
+    }
+}
