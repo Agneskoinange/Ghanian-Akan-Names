3471c4f2d1662bbb6f68065085c9c32f567cd306
Agnes <koinageagness@gmail.com>
2022-03-05 13:08:39 +0300

    create if/else statements in js

--- js/scripts.js
+++ js/scripts.js
@@ -7,35 +7,121 @@ var akanMaleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var akanFemaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
 //variables
-var  yearDigits = parseInt(year.substring(2,4));
+var YY= yearDigits
+var MM= monthOfBirth
+var DD= dayOfBirth
+var CC= centuryDigits
+var ValidMonth= monthValidator();
+var ValidDay= dayValidator();
+var d= daysOfTheWeek[]
+
+
+function generateAkanName() { 
+var  yearDigits = parseInt(year.slice(2,4));
 var  monthOfBirth = parseInt(document.getElementById("month").value);
 var  dayOfBirth = parseInt(document.getElementById("day").value);
-var  centryDigits = parseInt(year.substring(0,2));
+var  centuryDigits = parseInt(year.slice(0,2));
+ // how to calculate the day of the week that one is born
+ var d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+var d= daysOfTheWeek[]
+// Var ValidDay= dayValidator();
+var validMonth= monthValidator();
 
-var dayOfBirth= prompt("What is your year of Birth");
-var monthOfBirth = prompt("What is your year of Birth");
-var yearOfBirth = prompt("What is your year of Birth");
+//Therefore
 
-
-console.log();
-
-
-    function checkAkanName(){ 
-    YY = document.getElementById("year").value;
-    MM = document.getElementById("month").value;
-    DD = document.getElementById("day").value;
-    genders = document.getElementsByName("gender");
-    
-    // how to calculate the day of the week that one is born
-    let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-
-    
-    return(number)
- 
-    }
-    alert(d)
-
-
-
+if gender == ("male" || "famale") {
+   let akanName ="You were born on"+ "d" +"So Your name Ghanian Akan name is" + AkanName)
+}
 
 }
+else {
+  alert("Please select gender")
+}    
+
+if (day > 31 || day <1){
+  alert ("Invalid day")
+}
+// var validDay= dayValidator();
+else {
+  console.log (validDay)
+}
+
+
+if (month > 12 || month < 1){
+   alert ("Invalid month")
+}
+// var validMonth= monthValidator();
+else {
+  console.log (validMonth)
+}
+
+}
+
+
+var daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+
+var akanMaleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+
+     if gender == ("male" && "validDay" && "validMonth" && daysOfTheWeek[0]) {
+      let akanMaleNames ="You were born on"+ "daysOfTheWeek[0])" +"So Your name Ghanian Akan name is" + akanMaleNames[0];
+    
+     } else if 
+    gender == ("male" && "validDay" && "validMonth" && daysOfTheWeek[1]) {
+      let akanMaleNames ="You were born on"+ "daysOfTheWeek[1])" +"So Your name Ghanian Akan name is" + akanMaleNames[1];
+    
+    } else if 
+    gender == ("male" && "validDay" && "validMonth" && daysOfTheWeek[2]) {
+      let akanMaleNames ="You were born on"+ "daysOfTheWeek[2])" +"So Your name Ghanian Akan name is" + akanMaleNames[2];
+       
+    } else if 
+      gender == ("male" && "validDay" && "validMonth" && daysOfTheWeek[3]) {
+        let akanMaleNames ="You were born on"+ "daysOfTheWeek[3])" +"So Your name Ghanian Akan name is" + akanMaleNames[3];
+         
+    } else if 
+      gender == ("male" && "validDay" && "validMonth" && daysOfTheWeek[4]) {
+        let akanMaleNames ="You were born on"+ "daysOfTheWeek[4])" +"So Your name Ghanian Akan name is" + akanMaleNames[4];
+   
+    } else if 
+      gender == ("male" && "validDay" && "validMonth" && daysOfTheWeek[5]) {
+       let akanMaleNames ="You were born on"+ "daysOfTheWeek[5])" +"So Your name Ghanian Akan name is" + akanMaleNames[5];
+               
+    } else if 
+     gender == ("male" && "validDay" && "validMonth" && daysOfTheWeek[6]) {
+      let akanMaleNames ="You were born on"+ "daysOfTheWeek[6])" +"So Your name Ghanian Akan name is" + akanMaleNames[6];
+      
+    } else{
+    alert("Input your details again")
+    }
+   
+    var daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+
+    var akanFemaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+    
+    if gender == ("female" && "validDay" && "validMonth" && daysOfTheWeek[0]) {
+       let akanFemaleNames ="You were born on"+ "daysOfTheWeek[0])" +"So Your name Ghanian Akan name is" + akanFemaleNames[0];
+    
+    }else if gender == ("female" && "validDay" && "validMonth" && daysOfTheWeek[1]) {
+      let akanFemaleNames ="You were born on"+ "daysOfTheWeek[0])" +"So Your name Ghanian Akan name is" + akanFemaleNames[1];
+      
+
+    }else if gender == ("female" && "validDay" && "validMonth" && daysOfTheWeek[1]) {
+      let akanFemaleNames ="You were born on"+ "daysOfTheWeek[0])" +"So Your name Ghanian Akan name is" + akanFemaleNames[1];
+      
+
+    }else if gender == ("female" && "validDay" && "validMonth" && daysOfTheWeek[1]) {
+       let akanFemaleNames ="You were born on"+ "daysOfTheWeek[0])" +"So Your name Ghanian Akan name is" + akanFemaleNames[1];
+    
+
+    }else if gender == ("female" && "validDay" && "validMonth" && daysOfTheWeek[1]) {
+      let akanFemaleNames ="You were born on"+ "daysOfTheWeek[0])" +"So Your name Ghanian Akan name is" + akanFemaleNames[1];
+    
+
+    }else if gender == ("female" && "validDay" && "validMonth" && daysOfTheWeek[1]) {
+        let akanFemaleNames ="You were born on"+ "daysOfTheWeek[0])" +"So Your name Ghanian Akan name is" + akanFemaleNames[1];
+      }
+
+    } else {
+      alert("Input your details again")
+      }
+  
+      
